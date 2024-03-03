93bb53b0c2c0e07aafe78b7d5d330b1b724670da
SAMAR-QUEEN-BOSS <samaryasmine20@gmail.com>
2024-03-03 11:52:53 +0100

    EDIT

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GardienVie',
+      title: 'GardienVie',       
       // home: HomeScreen(),
       initialRoute: WelcomScreen.screenRoute,
       routes: {
