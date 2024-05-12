55394fd20d484ea4d0539547d6cded3e93c17a9b
SAMAR-QUEEN-BOSS <samaryasmine20@gmail.com>
2024-05-12 16:33:43 +0100

    edit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBuJ42C-cFPXVvGW9mwBJVFoqkAv_4R6f4")
+    GMSServices.provideAPIKey("AIzaSyAQPetWyDnSOJBePiCsEIRY6YKvtJ4-jjg")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -5,23 +5,62 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class MapScreen extends StatefulWidget {
   static const String screenRoute = 'map_screen';
 
-  const MapScreen({super.key});
+  const MapScreen({Key? key}) : super(key: key);
 
   @override
   State<MapScreen> createState() => MapScreenState();
 }
 
 class MapScreenState extends State<MapScreen> {
-  final user = FirebaseAuth.instance.currentUser!;
+  User? user;
+
+  @override
+  void initState() {
+    super.initState();
+    // Retrieve the current user when the widget initializes
+    user = FirebaseAuth.instance.currentUser;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('First Google Map'),
+        title: const Text(
+          'GardienVie',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(1000, 109, 12, 12),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              // app paramettre  + other users id and kick them
+            },
+            color: Colors.black,
+            icon: const Icon(
+              Icons.settings,
+              size: 40,
+            ),
+          ),
+        ],
       ),
-      body: const GoogleMap(
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            Row(
+              children: [Container()],
+            )
+          ],
+        ),
+      ),
+      body: GoogleMap(
         initialCameraPosition: CameraPosition(
-            target: LatLng(37.43296265331129, -122.08832357078792), zoom: 19),
+          target: LatLng(37.43296265331129, -122.08832357078792),
+          zoom: 19,
+        ),
       ),
     );
   }
